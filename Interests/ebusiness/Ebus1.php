dcb8479dd454a66ea4e6adb3e000c652d883675b
Mark Kenny <117477866@umail.ucc.ie>
2018-02-09 12:49:33 +0000

    9/02/18

--- Interests/ebusiness/Ebus1.php
+++ Interests/ebusiness/Ebus1.php
@@ -1,61 +1,57 @@
-<!DOCTYPE.html>
+<!DOCTYPE html>
 <html>
+    
     <head>
-        <title> Select Product</title>
+        <title>Select Product</title>
         
-        <!--JQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript" src "cost_calc.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>
+        </script>
+        
     </head>
-    
     <body>
-    
-    <h4>Select a Product</h4>
-    
-    <br/>
-    
-    <form method="POST" action="Ebus2.php">
+        <h4>Select a product</h4>
         
-        <label for "salesforce">
-          <input type="radio" id= "salesforce" name= "product" checked onClick="disablebtnProceed()"/>
-          SalesForce @ $100
-        </label>
+        </br>
         
-        <br/>
-        
-        <label for "aws">
-          <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
-          aws @ $300
-        </label>
-        
-        <br/>
-        <br/>
-        
-        <label for "Subtotal">
+        <form method="POST" action="Ebus2.php">
+            <label for="salesforce">
+                <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
+                Salesforce @ $100
+            </label>
             
-            Sub Total
-          <input type="text" id= "aws" subtotal= "product"  value="0.00" readonly/>
-          </label>
-          
-           <br/>
-           
-           <label for "Total">
-               
-           Total
-          <input type="text" id= "subtotal" name= "product"  value="0.00" readonly/>
-          
-        </label>
-        
-        <br/>
-        
-        <button type= "submit" id ="btnProceed" disabled> Add to Shopping Cart </button>
+            <br/>
+            
+            <label for="aws">
+                <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
+                AWS @ $300
+            </label>
+            
+            <br/>
+            <br/>
+            
+            <label for="subtotal">
+                Subtotal
+                <input type="text" id="subtotal" value="0.00" readonly/>
+            </label>
+            
+            <br/>
+            
+            <label for="total">
+                Subtotal
+                <input type="text" id="total" value="0.00" readonly/>
+            </label>
+            
+            <br/>
+            
+            <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
         
         </form>
         
         <br/>
         <button onClick="calcSub()">Calculate Cost</button>
-        <a role="button" href= "Ebus1.php">Clear Choice</a>
+        <a rule="button" href="Ebus1.php">Clear Choice</a>
         
-        </body>
         
-    </html>
+    </body>
+</html>
