02bd3e6ea755496bcb4817a206cc457a4ef2d1e9
Mark Kenny <117477866@umail.ucc.ie>
2018-02-25 18:37:11 +0000

    fixed external stylesheet

--- ebusiness/Ebus2.php
+++ ebusiness/Ebus2.php
@@ -11,55 +11,13 @@ session_start();
         <!--jQuery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script type="text/javascript" src="ebus2_validator.js"></script>
-   
+        
+    <meta charset = "UTF-8">
+         <link href="../stylesheet.css"
+         rel="stylesheet"
+          type= "text/css">
    </head>
-    <style>
-    
-    body
-                
-        .sidenav {
-            height: 100%;
-            width: 0;
-            position: fixed;
-            z-index: 1;
-            top: 0;
-            left: 0;
-            background-color: #111;
-            overflow-x: hidden;
-            transition: 0.5s;
-            padding-top: 60px;
-        }
-        
-        .sidenav a {
-            padding: 8px 8px 8px 32px;
-            text-decoration: none;
-            font-size: 25px;
-            color: #818181;
-            display: block;
-            transition: 0.3s;
-        }
-        
-        .sidenav a:hover {
-            color: #f1f1f1;
-        }
-        
-        .sidenav .closebtn {
-            position: absolute;
-            top: 0;
-            right: 25px;
-            font-size: 36px;
-            margin-left: 50px;
-        }
-        
-        @media screen and (max-height: 450px) {
-          .sidenav {padding-top: 15px;}
-          .sidenav a {font-size: 18px;}
-        }
-        span {
-            background-color: white;
-            border: solid black;
-        }
-            </style>
+
             <body>
                 
                        <div id="mySidenav" class="sidenav">
